413c1f0a4a4543126b63a9640f960237d99c8ca4
Agustin <equite.augusto@gmail.com>
2023-10-10 09:36:02 +0300

    deleted alchemy table

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const router = express.Router();
 
-app.use('/public', express.static('./public'));
+app.use('/Skyrim_Ingredients - The Unofficial Elder Scrolls Pages (UESP)_files', express.static('./Skyrim_Ingredients - The Unofficial Elder Scrolls Pages (UESP)_files'));
 
 app.get('/',function(req,res) {
     res.sendFile(__dirname + '/index.html');
